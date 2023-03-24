e722c3673a20f8a030e9a71cdc36b9a45719953b
AliceCaselli <alicecaselli3@gmail.com>
2023-03-24 03:33:55 +0100

    end

---
+++ src/components/AppHero.vue
@@ -0,0 +1,30 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+}
+</script>
+
+<template>
+    <div class="img-container">
+        <img src="public/images/jumbo.jpg" alt="">
+    </div>
+</template>
+
+<style>
+.img-container {
+    height: 500px;
+    width: 100%;
+    overflow: hidden;
+
+}
+
+.img-container img {
+    position: relative;
+    object-fit: cover;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+}
+</script>
+
+<template>
+    <h1> La mia prima app con Vite!</h1>
+</template>
+
+<style>
+h1 {
+    position: absolute;
+    top: 50px;
+    font-size: 3em;
+}
+</style>
